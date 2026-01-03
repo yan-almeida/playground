8cc3fc20d02d58c1e7f28ae4b651a1dbe628de57
yan-almeida <yanalmeidagarcia@gmail.com>
2026-01-03 19:02:49 -0300

    feat: implement QueuedCluster class for managing task messages with queuing and respawning

---
+++ src/cluster/queued-cluster.ts
@@ -0,0 +1,81 @@
+import { Serializable } from 'node:child_process';
+import { Cluster, InitializeCluster, InitializeQueuedCluster } from '.';
+import logger from '../logger';
+import { CheckSumGenerator } from '../utils/check-sum.generator';
+import { SafeJSONParse } from '../utils/json.parse';
+import { MaxNotifiableMap } from '../utils/max-notifiable.map';
+import { NotifiableMap } from '../utils/notifiable.map';
+import { ClusterMessage } from './cluster.message';
+import { TaskMessage } from './task.message';
+
+const MAX_ENTITIES = 1000;
+const MIN_ENTITIES = 10;
+
+export class QueuedCluster<ContentType> extends Cluster<TaskMessage<ContentType>, ClusterMessage<Serializable>> {
+  #queue: NotifiableMap<unknown>;
+
+  protected constructor(args: InitializeCluster<TaskMessage<ContentType>>, maxEntities: number = MIN_ENTITIES) {
+    super(args);
+
+    this.#queue = new MaxNotifiableMap<unknown>(Math.min(maxEntities, MIN_ENTITIES, MAX_ENTITIES)).whenReachMax(
+      this.#forceRespawn.bind(this),
+    );
+  }
+
+  static from<ContentType extends Serializable>({
+    maxEntities,
+    ...args
+  }: InitializeQueuedCluster): QueuedCluster<ContentType> {
+    const instance = new this(args, maxEntities) as QueuedCluster<ContentType>;
+
+    return instance;
+  }
+
+  send(message: Serializable): ClusterMessage<Serializable> {
+    const sent = this.internalSend(message);
+
+    const content = JSON.stringify(message);
+    const hash = CheckSumGenerator.toHash(Buffer.from(content));
+
+    if (!sent) {
+      this.#queue.set(hash, message);
+      logger.fork(`Message queued with hash: ${hash}`);
+    }
+
+    return new ClusterMessage({ key: hash, message });
+  }
+
+  #forceRespawn() {
+    const newSize = this.size - this.totalProcesses;
+
+    if (newSize <= 0) {
+      logger.fork('No processes need to be respawned - only requeueing');
+      this.#requeue();
+      return;
+    }
+
+    logger.fork(`Forcing respawn with old size: ${this.size}`);
+    logger.fork(`Forcing respawn with old processess size: ${this.totalProcesses}`);
+
+    this.initialize(newSize);
+
+    logger.fork(`Forcing respawn with new size: ${this.size}`);
+    logger.fork(`Forcing respawn with new processess size: ${this.totalProcesses}`);
+
+    this.#requeue();
+  }
+
+  #requeue(): void {
+    for (const [hash, message] of this.#queue.entries()) {
+      const parsed = SafeJSONParse(message);
+      const sent = this.send(parsed);
+
+      if (!sent) {
+        continue;
+      }
+
+      this.#queue.delete(hash);
+      logger.fork(`Requeued message with key: ${hash}`);
+    }
+  }
+}
