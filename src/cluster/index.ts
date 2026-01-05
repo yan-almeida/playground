5c62ef9a64563497b3cbac16c585272b743b71d8
yan-almeida <yanalmeidagarcia@gmail.com>
2026-01-05 20:30:32 -0300

    refactor: format ClusterCallback type definition and add InitializeRetryableCluster interface

--- src/cluster/index.ts
+++ src/cluster/index.ts
@@ -1,6 +1,10 @@
 import { Serializable } from 'node:child_process';
+import { RetryConfig } from './retry-strategy';
 
-export type ClusterCallback<Message = Serializable> = (pid: number, message: Message) => void | Promise<void>;
+export type ClusterCallback<Message = Serializable> = (
+  pid: number,
+  message: Message,
+) => void | Promise<void>;
 
 export interface InitializeCluster<Message = Serializable> {
   file: string;
@@ -8,17 +12,21 @@ export interface InitializeCluster<Message = Serializable> {
   callback: ClusterCallback<Message>;
 }
 
-export interface InitializeQueuedCluster<Message = Serializable> extends InitializeCluster<Message> {
+export interface InitializeQueuedCluster<Message = Serializable>
+  extends InitializeCluster<Message> {
   maxEntities?: number;
 }
 
-export interface InitializeAutoRecCluster<Message = Serializable> extends InitializeCluster<Message> {
+export interface InitializeRetryableCluster<Message = Serializable>
+  extends InitializeQueuedCluster<Message> {
+  retryConfig?: Partial<RetryConfig>;
+}
+
+export interface InitializeAutoRecCluster<Message = Serializable>
+  extends InitializeCluster<Message> {
   disableAutoRecovery?: true;
 }
 
 export enum ClusterSignal {
   RESPAWNABLE = 999901,
 }
-
-export * from './cluster';
-export * from './queued-cluster';
