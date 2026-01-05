64791f328e53119368c94d4af06d019047b7def8
yan-almeida <yanalmeidagarcia@gmail.com>
2026-01-05 20:33:09 -0300

    feat: add RetryableTaskMessage class with state management and constructor

--- src/cluster/task.message.ts
+++ src/cluster/task.message.ts
@@ -1,12 +1,29 @@
 import { SafeJSONParse } from '../utils/json.parse';
 import { ClusterMessage } from './cluster.message';
+import { RetryState } from './retry-strategy';
 
 export class TaskMessage<Content> extends ClusterMessage<Content> {
-  static from<Content>(message: ClusterMessage<Content> | string): TaskMessage<Content> {
+  static from<Content>(
+    message: ClusterMessage<Content> | string,
+  ): TaskMessage<Content> {
     if (typeof message === 'string') {
       return new this(SafeJSONParse(message));
     }
 
     return new this({ key: message.key, message: message.message });
+  }
+}
+
+export class RetryableTaskMessage<Content> extends ClusterMessage<Content> {
+  state: Readonly<RetryState>;
+
+  constructor(
+    args: Omit<
+      RetryableTaskMessage<Content>,
+      typeof Symbol.toPrimitive | 'instanceof' | '__name'
+    >,
+  ) {
+    super(args);
+    this.state = args.state;
   }
 }
