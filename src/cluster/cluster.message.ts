bf506103bfa0c4843264ecd0b2af55d459b1615e
yan-almeida <yanalmeidagarcia@gmail.com>
2026-01-05 20:23:03 -0300

    refactor: update __name property to be explicitly typed as Readonly<string>

--- src/cluster/cluster.message.ts
+++ src/cluster/cluster.message.ts
@@ -1,17 +1,26 @@
 import { Serializable } from 'node:child_process';
 
 export class ClusterMessage<Message = Serializable> {
-  readonly __name = this.constructor.name;
+  __name: Readonly<string> = this.constructor.name;
 
   message: Message;
   key: string;
 
-  constructor(args: Omit<ClusterMessage<Message>, typeof Symbol.toPrimitive | '__name'>) {
+  constructor(
+    args: Omit<
+      ClusterMessage<Message>,
+      typeof Symbol.toPrimitive | 'instanceof' | '__name'
+    >,
+  ) {
     this.message = args.message;
     this.key = args.key;
   }
 
   protected [Symbol.toPrimitive]() {
     return JSON.stringify(this);
+  }
+
+  static instanceof<Message = any>(args: any): args is ClusterMessage<Message> {
+    return !!args.__name || args instanceof ClusterMessage;
   }
 }
