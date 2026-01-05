13fcc14db3a750595d787910fb3ceace8c5d6bc5
yan-almeida <yanalmeidagarcia@gmail.com>
2026-01-05 20:33:34 -0300

    feat: add retryableFork, retryStrategy, and retryScheduler to logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,10 +2,23 @@ import debug from 'debug';
 
 const fork = debug('cluster:fork');
 const ackeableFork = debug('cluster:ackeable-fork');
+const retryableFork = debug('cluster:retryable-fork');
 
 const task = debug('cluster:task');
 const emitter = debug('set:emitter');
 
 const balancer = debug('cluster:balancer');
 
-export default { fork, task, balancer, emitter, ackeableFork };
+const retryStrategy = debug('cluster:retry-strategy');
+const retryScheduler = debug('cluster:retry-scheduler');
+
+export default {
+  fork,
+  task,
+  balancer,
+  emitter,
+  ackeableFork,
+  retryableFork,
+  retryStrategy,
+  retryScheduler,
+};
